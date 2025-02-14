b8e3d30e8523e024368f955f49ca20a4d9b0b870
AMIT PATHAK <146617386+AmitPathak49@users.noreply.github.com>
2025-02-14 17:55:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburgerIcon = document.querySelector('.hamburger-menu-container')
+const headerContent = document.querySelector('.header-content')
+const closeIcon = document.querySelector('.close-icon')
+
+
+hamburgerIcon.addEventListener('click', () => {
+    headerContent.classList.add('menu-open')
+})
+
+closeIcon.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
+
+closeIcon.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
